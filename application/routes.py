b3f8901f40ad2a3061bbf0fce7f30aacc39e902d
Tim Fredlund <tim@timpowered.com>
2022-02-14 16:02:02 -0600

    standardize on helpers

--- application/routes.py
+++ application/routes.py
@@ -25,6 +25,36 @@ from email.utils import formatdate
 from calendar import timegm
 
 ### helpers
+def is_valid_user(username: str):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        return None
+    return user
+
+def is_authorized_user(request_username: str, auth_username: str):
+    user = is_valid_user(request_username)
+    if user is None or user.username != auth_username:
+        return None
+    return user
+
+def is_modified(request_header: str, timestamp: datetime):
+    if request_header is not None:
+        return parsedate(request_header).replace(tzinfo=None) < timestamp
+    return True
+
+def unmodified_response(timestamp: datetime):
+    response = Response(None, status=304)
+    response.headers['Last-Modified'] = formatdate(timeval=timegm(timestamp.timetuple()), localtime=False, usegmt=True)
+    return response
+
+def invalid_request_response(error: str = "Invalid Request", code: int = 400):
+    return Response(error, code)
+
+def unauthorized_response():
+    return Response("Unauthorized Access", status=401)
+
+def missinguser_response():
+    return Response("No such user", status=404)
 
 def update_user_timestamp(user: User):
     user.last_updated = datetime.utcnow().replace(microsecond=0)
@@ -38,14 +68,10 @@ def is_valid_username(username: str):
 @app.route('/.well-known/fmrl/user/<username>/avatar', methods=['PUT'])
 @httpauth.login_required
 def update_user_avatar(username: str):
-
-    # Request Validation
-    user = User.query.filter_by(username=username).first()
+    user = is_authorized_user(username, httpauth.current_user().username)
     if user is None:
-        return Response("No such user found.", status=404)
-    if user.username != httpauth.current_user().username:
-        return Response("Unauthenticated.", status=403)
-    
+        return unauthorized_response()
+        
     # Grab first chunk for MIME analysis
     file_size = 0
     chunk_size = 4096
@@ -56,7 +82,7 @@ def update_user_avatar(username: str):
 
     # Validate file magic
     if file_type not in app.config['ALLOWED_UPLOAD_TYPES']:
-        return Response("Image not recognized as JPEG or PNG.", status=400)
+        return invalid_request_response("Image not recognized as JPEG or PNG")
 
     # Read image into buffer and validate file size.
     while True:
@@ -74,7 +100,7 @@ def update_user_avatar(username: str):
     
     img = Image.open(image_bytes)
     if img.height != img.width:
-        return Response("Image must be square.", status=400)
+        return invalid_request_response("Image must be square")
     
     # Stream buffer back to file on disk
     # TODO - Beef up security here
@@ -90,35 +116,31 @@ def update_user_avatar(username: str):
 @app.route('/.well-known/fmrl/user/<username>', methods=['PATCH'])
 @httpauth.login_required
 def update_user_status(username: str):
-    
-    # Request Validation
-    user = User.query.filter_by(username=username).first()
+    user = is_authorized_user(username, httpauth.current_user().username)
     if user is None:
-        return Response("No such user found.", status=404)
-    if user.username != httpauth.current_user().username:
-        return Response("Unauthenticated.", status=403)
+        return unauthorized_response()
     
     # Schema Validation
     if not JsonLengthInputs(request).validate():
         return Response("Update field(s) too long!", status=413)
     if not JsonTypeInputs(request).validate():
-        return Response("Update fields not a valid type.", status=400)
+        return invalid_request_response("Update fields not a valid type")
     
     # Semantic Validation
     update = {} 
     if request.json.get('avatar') is not None:
-        return Response("Invalid endpoint for updating avatar.", status=400)
+        return invalid_request_response("Invalid endpoint for updating avatar")
     
     if request.json.get('emoji') is not None:
         if not request.json['emoji'] in EmojiSequence and request.json['emoji']:
-            return Response("Invalid emoji.", status=400)
+            return invalid_request_response("Invalid emoji")
         update['emoji'] = request.json['emoji']
 
     regex = re.compile('[\u0000-\u001F\u007F\u0080-\u009F]')
     for field in {"status", "media", "name"}:
         if request.json.get(field) is not None:
             if regex.search(request.json.get(field)) is not None:
-                return Response("Invalid unicode character(s).", status=400)
+                return invalid_request_response("Invalid unicode character(s)")
             update[field] = request.json[field]
     
     if request.json.get('media_type') is not None:
@@ -126,7 +148,7 @@ def update_user_status(username: str):
 
     if request.json.get('uri') is not None:
         if not is_valid_uri(request.json['uri']) or len(request.json['uri'].encode('utf-8') > 500):
-            return Response("Invalid URI.", status=400)
+            return invalid_request_response("Invalid URI")
         update['uri'] = normalize_uri(request.json['uri'])
 
     # Validation passed. Update user and times
@@ -142,7 +164,7 @@ def update_user_status(username: str):
 def get_user_statuses():
     # Basic request validation
     if not request.args.getlist('user'):
-        return Response("Invalid request.", status=400)
+        return invalid_request_response()
 
     # If requested, send only updates newer than specified
     query_time = None
@@ -173,17 +195,12 @@ def get_user_statuses():
     return Response(json.dumps(users_list), status=200, mimetype='application/json')
 
 def get_user_status(username: str, query_time=None):
-    user = User.query.filter_by(username=username).first()
-    if not user:
-        # Return failed response (user doesn't exist)
-        return Response("No such user found.", status=404)
+    user = is_valid_user(username=username)
+    if user is None:
+        return missinguser_response()
     # If requested, send only updates newer than specified
-    if request.headers.get('If-Modified-Since') is not None:
-        query_time = parsedate(request.headers['If-Modified-Since']).replace(tzinfo=None)
     if query_time is not None and query_time >= user.last_updated:
-        response = Response(None, status=304)
-        response.headers['Last-Modified'] = formatdate(timeval=timegm(user.last_updated.timetuple()), localtime=False, usegmt=True)
-        return response
+        return unmodified_response(user.last_updated)
     
     # Empty responses are valid, should return only values which are set
     user_status = {}
@@ -204,19 +221,12 @@ def get_user_status(username: str, query_time=None):
 @app.route('/.well-known/fmrl/user/<username>/following', methods=['GET'])
 @httpauth.login_required
 def get_user_following(username):
-    user = User.query.filter_by(username=username).first()
+    user = is_authorized_user(username, httpauth.current_user().username)
     if user is None:
-        return Response("No such user found.", status=404)
-    if user.username != httpauth.current_user().username:
-        return Response("Unauthenticated.", status=403)
+        return unauthorized_response()
 
-    # If requested, send only updates newer than specified
-    if request.headers.get('If-Modified-Since') is not None:
-        query_time = parsedate(request.headers['If-Modified-Since']).replace(tzinfo=None)
-    if query_time is not None and query_time >= user.follows_updated:
-        response = Response(None, status=304)
-        response.headers['Last-Modified'] = formatdate(timeval=timegm(user.follows_updated.timetuple()), localtime=False, usegmt=True)
-        return response
+    if not is_modified(request.headers.get('If-Modified-Since'), user.follows_updated):
+        return unmodified_response(user.follows_updated)
 
     user_follows = []
     if user.follows is not None:
@@ -231,18 +241,16 @@ def get_user_following(username):
 @app.route('/.well-known/fmrl/user/<username>/following', methods=['PATCH'])
 @httpauth.login_required
 def set_user_following(username: str):
-    user = User.query.filter_by(username=username).first()
+    user = is_authorized_user(username, httpauth.current_user().username)
     if user is None:
-        return Response("No such user found.", status=404)
-    if user.username != httpauth.current_user().username:
-        return Response("Unauthenticated.", status=403)
+        return unauthorized_response()
     
     success = False
     for action in {"add", "remove"}:
         if type(request.json.get(action)) is list:
             for name in request.json[action]:
                 if not is_valid_username(name):
-                    return Response("Invalid username(s).", status=400)
+                    return invalid_request_response("Invalid username(s)")
                 if action == "add":
                     success = True
                     if user.follows.filter_by(username=name).first() is None:
@@ -257,7 +265,7 @@ def set_user_following(username: str):
         db.session.commit()
         return Response(None, status=200)
     else:
-        return Response("Invalid request.", status=400)
+        return invalid_request_response()
 
 ### flashpaper routes
 
@@ -265,9 +273,9 @@ def set_user_following(username: str):
 @cross_origin()
 @app.route('/.well-known/fmrl/avatars/<username>', methods=['GET'])
 def get_user_avatar(username: str):
-    user = User.query.filter_by(username=username).first()
+    user = is_valid_user(username=username)
     if user is None:
-        return Response("No such user found.", status=404)
+        return missinguser_response()
     image_path = safe_join(app.config['AVATARS_DIR'], username)
     if path.exists(image_path):
         mime_type = magic.from_file(image_path, mime=True)
@@ -281,4 +289,4 @@ def get_user_avatar(username: str):
 @app.route('/.well-known/fmrl/', defaults={'path': ''})
 @app.route('/.well-known/fmrl/<path:path>')
 def invalid_endpoint(path):
-    return Response("Invalid API endpoint.", status=405)
+    return invalid_request_response("Invalid API endpoint", 405)
